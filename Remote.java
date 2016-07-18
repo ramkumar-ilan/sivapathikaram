7955331a03f25a04a808b3dd8edd0b3ba5a395ba
irakulal <ramkumar.lal.trichy@gmail.com>
2016-07-18 19:04:24 +0530

    Create Remote.java

---
+++ Remote.java
@@ -0,0 +1,30 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+
+public class remote {
+	private Selenium selenium;
+
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "https://www.google.co.in/");
+		selenium.start();
+	}
+
+	@Test
+	public void testRemote() throws Exception {
+		selenium.open("/search?q=how+to+record+replay+in+selenium+IDE+firefox&ie=utf-8&oe=utf-8&client=firefox-b-ab&gfe_rd=cr&ei=0AhhV9nCC86L8QfIlbKAAQ");
+		selenium.click("link=Selenium IDE Plugins");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
