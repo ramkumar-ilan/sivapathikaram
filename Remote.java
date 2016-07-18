1de90272b372db4ebec497359f94d09e6fa3d28e
irakulal <ramkumar.lal.trichy@gmail.com>
2016-07-18 19:05:09 +0530

    Update Remote.java

--- Remote.java
+++ Remote.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 
 public class remote {
 	private Selenium selenium;
